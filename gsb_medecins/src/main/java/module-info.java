ba154915e27294e2d9741f8bc3931b9fb175d49b
Faustine Y <79989005+FaustineYvert@users.noreply.github.com>
2022-03-23 16:28:37 +0100

    Delete gsb_medecins directory
    (Reformatting the repo)

--- gsb_medecins/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module fr.faustine.gsb_medecins {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens fr.faustine.gsb_medecins to javafx.fxml;
-    exports fr.faustine.gsb_medecins;
-}
